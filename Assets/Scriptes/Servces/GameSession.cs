b0df41ec9bb7d0266d1587ef16bbbb9a14f72de4
Kapitan999 <Lavreckiy.ka@gmail.com>
2023-04-24 22:47:10 +0300

    Upd 8

--- Assets/Scriptes/Servces/GameSession.cs
+++ Assets/Scriptes/Servces/GameSession.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class GameSession : MonoBehaviour
 {
     [SerializeField] private int _levelIndex;
-    
+
+
+    private Transform player;
+    private Transform finish;
+
+    private void Update()
+    {
+        //splayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        //finish = GameObject.FindGameObjectWithTag("Finish").GetComponent<Transform>();
+
+        if (player.position == finish.position)
+        {
+            
+        }
+    }
 
     public int LevelComplete => _levelIndex;
     private void Awake() {
--- Assets/Servises/TMPFiles/EnterTriggerTMP.cs
+++ Assets/Servises/TMPFiles/EnterTriggerTMP.cs
@@ -8,11 +8,13 @@ public class EnterTriggerTMP : MonoBehaviour
     [SerializeField] private string _tag;
     [SerializeField] private EnterEvent _action;
     
-
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void OnCollisionStay(Collision collision)
     {
-        if(!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return;
-
-        _action?.Invoke(other.gameObject);
+        if (!string.IsNullOrEmpty(_tag) && collision.gameObject.CompareTag(_tag)) return;
+        {
+            _action?.Invoke(collision.gameObject);
+            Debug.Log("YEs");
+        }
+        Debug.Log("NI");
     }
 }
