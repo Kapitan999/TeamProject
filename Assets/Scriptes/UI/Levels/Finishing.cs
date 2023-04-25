1e5850d9923d2e3f9354bb95425afb5da4a7a087
Sawolfer <Savipon@yandex.ru>
2023-04-25 12:30:45 +0300

    upd 10

--- Assets/Scriptes/UI/Levels/Finishing.cs
+++ Assets/Scriptes/UI/Levels/Finishing.cs
@@ -7,16 +7,25 @@ public class Finishing : MonoBehaviour
 {
     private GameSession _session;
 
+    public GameObject _finish;
+    public GameObject _player;
+
     private void Awake() {
         _session = FindObjectOfType<GameSession>();
-    
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _finish = GameObject.FindGameObjectWithTag("Finish");
     }
-
+    
 
     [ContextMenu("Finish")]
     public void OnFinishLevel(){
-        _session.OnLevelComplite();
-        SceneManager.LoadScene("LevelsWindow");
+        Vector3 tmp = new Vector3 (_finish.transform.position.x, _finish.transform.position.y - 0.5f, 0);
+        if (_player.transform.position == tmp){
+            Debug.Log("111");
+
+            _session.OnLevelComplite();
+            SceneManager.LoadScene("LevelsWindow");
+        }
     }
 
 }
